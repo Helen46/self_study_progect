da7701b8d246f7392aa6cee0c68a24a0ddb5616e
Elena Prilutskay <a.priluckay87@gmail.com>
2025-01-04 23:07:56 +0300

    add views, serializers and urls from model Lesson

--- courses/urls.py
+++ courses/urls.py
@@ -1,12 +1,14 @@
+from django.urls import path
 from rest_framework.routers import SimpleRouter
 
 from courses.apps import CoursesConfig
-from courses.views import CourseViewSet
+from courses.views import CourseViewSet, LessonViewSet
 
 app_name = CoursesConfig.name
 
 router = SimpleRouter()
-router.register("", CourseViewSet)
+router.register(r'courses', CourseViewSet, basename='courses')
+router.register(r'lessons', LessonViewSet, basename='lessons')
 
 urlpatterns = []
 
--- courses/views.py
+++ courses/views.py
@@ -1,9 +1,15 @@
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 from rest_framework.viewsets import ModelViewSet
 
-from courses.models import Course
-from courses.serializers import CourseSerializer
+from courses.models import Course, Lesson
+from courses.serializers import CourseSerializer, LessonSerializer
 
 
 class CourseViewSet(ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+
+class LessonViewSet(ModelViewSet):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
