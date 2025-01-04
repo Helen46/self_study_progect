2065889311b734f6aea4b29ef9ac3e73209029c4
Elena Prilutskay <a.priluckay87@gmail.com>
2025-01-04 21:34:26 +0300

    add class Lesson

--- courses/models.py
+++ courses/models.py
@@ -2,17 +2,18 @@ from django.db import models
 
 from config.settings import NULLABLE
 
-
-class Course(models.Model):
-    """
-    Модель курса
-    """
-    STATUS_OF_PASSAGE = [
+STATUS_OF_PASSAGE = [
         ('available', 'доступен'),
         ('unavailable', 'недоступен'),
         ('in progress', 'в процессе'),
         ('passed', 'пройден')
     ]
+
+
+class Course(models.Model):
+    """
+    Модель курса
+    """
     name = models.CharField(
         max_length=300,
         verbose_name='Название курса',
@@ -32,6 +33,51 @@ class Course(models.Model):
     class Meta:
         verbose_name = 'Курс'
         verbose_name_plural = 'Курсы'
+
+    def __str__(self):
+        return self.name
+
+
+class Lesson(models.Model):
+    """
+    Модель урока
+    """
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.SET_NULL,
+        verbose_name='Курс',
+        **NULLABLE
+    )
+    name = models.CharField(
+        max_length=300,
+        verbose_name='Название урока',
+        help_text='Добавьте название урока',
+    )
+    lesson_content = models.TextField(
+        verbose_name='Содержание урока',
+        help_text='Добавьте содержание урока',
+    )
+    video = models.URLField(
+        max_length=300,
+        verbose_name='Ссылка на видео урока',
+        help_text='Добавьте ссылку на видео урока',
+        **NULLABLE
+    )
+    image = models.ImageField(
+        upload_to='courses/images',
+        verbose_name='Изображение',
+        help_text='Добавьте изображение',
+        **NULLABLE
+    )
+    status = models.CharField(
+        max_length=16,
+        choices=STATUS_OF_PASSAGE,
+        verbose_name='Статус урока',
+        default='available'
+    )
+    class Meta:
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
 
     def __str__(self):
         return self.name
